fb44e05349e43365c196cbca850c36789468b993
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-29 12:49:00 -0400

    Added DELETE to Actions

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -48,4 +48,18 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await actions
+      .getActions()
+      .where({ id })
+      .del();
+    return count
+      ? res.status(200).json(count)
+      : res.status(404).json({ message: "No Action found to delete at ID" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Error", err });
+  }
+});
 module.exports = router;
